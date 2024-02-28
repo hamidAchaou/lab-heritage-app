cba68112ad3d97d37f51c238c77fc6c27f5bbc7a
Hamid Achaou <hamidAchaou379@gmail.com>
2024-02-28 16:06:42 +0100

    add migration

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Illuminate\Database\MigrationServiceProvider::class,
+
     ])->toArray(),
 
     /*
